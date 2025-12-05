9a9c541904d896c230c9a8b2ca1293cf398321fb
yaswanth63796 <yeshuv24@gmail.com>
2025-12-05 08:54:18 +0530

    file added

---
+++ index.js
@@ -0,0 +1,36 @@
+let input=document.getElementById('input')
+let todolist=document.getElementById('todolist')
+let button=document.getElementById('btn')
+
+let todos=[]
+window.onload=()=>{
+   todos=JSON.parse(localStorage.getItem('todos'))||[]
+   todos.forEach(todo=>display(todo))
+}
+
+button.addEventListener('click',()=>{
+       todos.push(input.value)
+       display(input.value)
+       input.value=''
+
+})
+
+function display(todo){
+    let para=document.createElement('p')
+    para.innerText=todo
+    todolist.appendChild(para)
+    localStorage.setItem('todos',JSON.stringify(todos)) 
+    para.addEventListener('dblclick',()=>{
+        para.style.textDecoration='line through'
+        todolist.removeChild(para)
+          remove(todo)
+    })  
+}
+
+function remove(todo){
+  let index=  todos.indexOf(todo)
+  if(index>-1)
+    todos.splice(index,1)
+localStorage.setItem('todos',JSON.stringify(todos))  
+      
+}
